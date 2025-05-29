3734d5ed60a828fb5ac86c71b7fc26829a503a6e
jagadeesh <jagadeesh.m@autodesk.com>
2025-05-30 03:34:59 +0530

    new test

--- source/utils/greet.ts
+++ source/utils/greet.ts
@@ -1,3 +1,18 @@
 export function greet(name: string): string {
 	return `Hello, ${name}!`;
 }
+
+interface User {
+	username: string;
+	password: string;
+}
+
+const mockUserDB: User = {
+	username: "admin",
+	password: "password123",
+};
+
+export function login(username: string, password: string): boolean {
+	return username === mockUserDB.username && password === mockUserDB.password;
+}
+
